d2d0838c354812650ad03a38064bcfb6ec58677d
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-11-04 11:21:32 +0800

    Add departments mock data

---
+++ data.sql
@@ -0,0 +1,11 @@
+-- departments
+INSERT INTO departments (did, dname) VALUES (0, 'Human Resources');
+INSERT INTO departments (did, dname) VALUES (1, 'Product Management');
+INSERT INTO departments (did, dname) VALUES (2, 'Research and Development');
+INSERT INTO departments (did, dname) VALUES (3, 'Sales');
+INSERT INTO departments (did, dname) VALUES (4, 'Support');
+INSERT INTO departments (did, dname) VALUES (5, 'Legal');
+INSERT INTO departments (did, dname) VALUES (6, 'Engineering');
+INSERT INTO departments (did, dname) VALUES (7, 'Training');
+INSERT INTO departments (did, dname) VALUES (8, 'Marketing');
+INSERT INTO departments (did, dname) VALUES (9, 'Accounting');
