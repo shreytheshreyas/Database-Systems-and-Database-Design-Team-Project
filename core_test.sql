606eb0679d5a9fa6bb0ce71a8721956f9bd07354
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-11-07 05:59:09 +0800

    Add tests for leave_meeting

--- core_test.sql
+++ core_test.sql
@@ -18,5 +18,319 @@ SELECT unbook_room(3,1,'2021-11-15','1400','1600',14); -- unbooking a non - exis
 SELECT join_meeting (2, 3, '2021-11-21', '1100', '1600', 21); -- joining an existant unapproved meeting
 SELECT join_meeting (2, 4,  '2021-11-22', '1100', '1600', 21); -- joining a non-existant meeting
 SELECT join_meeting (3, 3,  '2021-11-21', '0900', '1200', 21); -- meeting is never booked the employee eid at line 11 has resigned from the company
-SELECT join_meeting (3, 3, '2021-11-15', '1400', '1600',3); --shouldn't be able to join because of the employee is taking part in another meeting 
--- 
+SELECT join_meeting (3, 3, '2021-11-15', '1400', '1600',3); --shouldn't be able to join because of the employee is taking part in another meeting
+
+/**
+ * leave_meeting test cases
+ */
+SELECT '##### Test cases for leave_meeting #####';
+
+-- non-existent employee
+SELECT 'Test case: non-existent employee';
+SELECT 'Expected error: This employee does not exist.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '9:00'::TIME, '12:00'::TIME, 61);
+
+-- retired employee
+SELECT 'Test case: retired employee';
+SELECT 'Expected error: This employee is already retired.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '9:00'::TIME, '12:00'::TIME, 60);
+
+-- not on-the-hour start time
+SELECT 'Test case: not on-the-hour start time';
+SELECT 'Expected error: All hours must be exactly on the hour.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '9:01'::TIME, '12:00'::TIME, 20);
+SELECT 'Expected error: All hours must be exactly on the hour.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '8:59'::TIME, '12:00'::TIME, 20);
+
+-- not on-the-hour end time
+SELECT 'Test case: not on-the-hour end time';
+SELECT 'Expected error: All hours must be exactly on the hour.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '9:00'::TIME, '12:01'::TIME, 20);
+SELECT 'Expected error: All hours must be exactly on the hour.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '9:00'::TIME, '11:59'::TIME, 20);
+
+-- non-existent meeting
+SELECT 'Test case: non-existent meeting';
+SELECT 'Expected error: This meeting does not exist.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '9:00'::TIME, '13:00'::TIME, 20);
+SELECT 'Expected error: This meeting does not exist.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '8:00'::TIME, '12:00'::TIME, 20);
+
+-- already approved meeting
+SELECT 'Test case: already approved meeting';
+SELECT 'Expected error: None of the sessions in the meeting must already be approved.';
+SELECT leave_meeting(2, 1, '8/11/2021'::DATE, '9:00'::TIME, '12:00'::TIME, 20);
+
+-- not joining meeting at all
+SELECT 'Test case: not joining meeting at all';
+SELECT 'Expected result: Successfully return after doing nothing.';
+SELECT leave_meeting(1, 1, '15/11/2021'::DATE, '15:00'::TIME, '16:00'::TIME, 20);
+
+-- joining part of but not entire meeting
+SELECT 'Test case: joining part of but not entire meeting';
+SELECT 'Precondition: Session (floor 1, room 1, 17/11/2021, 1800, booker id 2) '
+        'and (floor 1, room 1, 17/11/2021, 1900, booker id 10) exist';
+SELECT book_room(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 2);
+SELECT book_room(1, 1, '17/11/2021'::DATE, '19:00'::TIME, '20:00'::TIME, 10);
+SELECT
+    CASE
+        is_existing_meeting(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Precondition: Join (floor 1, room 1, 17/11/2021, 1800, joiner id 2) '
+        'and (floor 1, room 1, 17/11/2021, 1900, joiner id 2) does not exist';
+SELECT
+    CASE
+        NOT is_joining_session(1, 1, '17/11/2021'::DATE, '19:00'::TIME, 2)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected error: If the employee was originally joining some session within the duration, '
+        'then they must have been originally joining all the sessions within the duration '
+        'to leave them all.';
+SELECT leave_meeting(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 2);
+SELECT 'Tear down: Remove session (floor 3, room 4, 17/11/2021, 1800, booker id 2) '
+        'and (floor 3, room 4, 17/11/2021, 1900, booker id 10)';
+SELECT unbook_room(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 2);
+SELECT unbook_room(1, 1, '17/11/2021'::DATE, '19:00'::TIME, '20:00'::TIME, 10);
+SELECT
+    CASE
+        NOT is_existing_session(1, 1, '17/11/2021'::DATE, '18:00'::TIME)
+        AND NOT is_existing_session(1, 1, '17/11/2021'::DATE, '19:00'::TIME)
+    WHEN TRUE
+        THEN 'Tear down successful.'
+    ELSE
+        'Tear down unsuccessful.'
+    END;
+
+-- booking part of but not entire meeting
+SELECT 'Test case: booking part of but not entire meeting';
+SELECT 'Precondition: Session (floor 1, room 1, 17/11/2021, 1800, booker id 2) '
+        'and (floor 1, room 1, 17/11/2021, 1900, booker id 10) exist';
+SELECT book_room(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 2);
+SELECT book_room(1, 1, '17/11/2021'::DATE, '19:00'::TIME, '20:00'::TIME, 10);
+SELECT
+    CASE
+        is_existing_meeting(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Precondition: Join (floor 1, room 1, 17/11/2021, 1800, joiner id 2) '
+        'and (floor 1, room 1, 17/11/2021, 1900, joiner id 2) exist';
+SELECT join_meeting(1, 1, '17/11/2021'::DATE, '19:00'::TIME, '20:00'::TIME, 2);
+SELECT
+    CASE
+        is_joining_entire_duration(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 2)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected error: If the employee is the booker of some session within the duration, '
+        'then they must be the booker of all the sessions within the duration '
+        'to leave them all.';
+SELECT leave_meeting(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 2);
+SELECT 'Tear down: Remove session (floor 3, room 4, 17/11/2021, 1800, booker id 2) '
+        'and (floor 3, room 4, 17/11/2021, 1900, booker id 10)';
+SELECT unbook_room(1, 1, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 2);
+SELECT unbook_room(1, 1, '17/11/2021'::DATE, '19:00'::TIME, '20:00'::TIME, 10);
+SELECT
+    CASE
+        NOT is_existing_session(1, 1, '17/11/2021'::DATE, '18:00'::TIME)
+        AND NOT is_existing_session(1, 1, '17/11/2021'::DATE, '19:00'::TIME)
+    WHEN TRUE
+        THEN 'Tear down successful.'
+    ELSE
+        'Tear down unsuccessful.'
+    END;
+
+-- leave one session
+SELECT 'Test case: leave one session';
+SELECT 'Precondition: Session (floor 3, room 4, 17/11/2021, 1800, booker id 12) exists';
+SELECT book_room(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 12);
+SELECT
+    CASE
+        is_existing_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Precondition: Join (floor 3, room 4, 17/11/2021, 1800, joiner id 45) exists';
+SELECT join_meeting(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 45);
+SELECT
+    CASE
+        is_joining_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME, 45)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected result: Successfully return after leaving one session.';
+SELECT leave_meeting(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 45);
+SELECT 'Postcondition: Join (floor 3, room 4, 17/11/2021, 1800, joiner id 45) does not exist';
+SELECT
+    CASE
+        NOT is_joining_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME, 45)
+    WHEN TRUE
+        THEN 'Postcondition met.'
+    ELSE
+        'Postcondition not met.'
+    END;
+SELECT 'Tear down: Remove session (floor 3, room 4, 17/11/2021, 1800, booker id 12)';
+SELECT unbook_room(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 12);
+SELECT
+    CASE
+        NOT is_existing_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME)
+    WHEN TRUE
+        THEN 'Tear down successful.'
+    ELSE
+        'Tear down unsuccessful.'
+    END;
+
+-- leave multiple sessions
+SELECT 'Test case: leave multiple sessions';
+SELECT 'Precondition: Session (floor 3, room 4, 17/11/2021, 1800, booker id 12) '
+        'and (floor 3, room 4, 17/11/2021, 1900, booker id 12) exist';
+SELECT book_room(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 12);
+SELECT
+    CASE
+        is_existing_meeting(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Precondition: Join (floor 3, room 4, 17/11/2021, 1800, joiner id 45) '
+        'and (floor 3, room 4, 17/11/2021, 1900, joiner id 45) exist';
+SELECT join_meeting(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 45);
+SELECT
+    CASE
+        is_joining_entire_duration(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 45)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected result: Successfully return after leaving multiple sessions.';
+SELECT leave_meeting(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 45);
+SELECT 'Postcondition: Join (floor 3, room 4, 17/11/2021, 1800, joiner id 45) '
+        'and (floor 3, room 4, 17/11/2021, 1900, joiner id 45) do not exist';
+SELECT
+    CASE
+        NOT is_joining_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME, 45)
+        AND NOT is_joining_session(3, 4, '17/11/2021'::DATE, '19:00'::TIME, 45)
+    WHEN TRUE
+        THEN 'Postcondition met.'
+    ELSE
+        'Postcondition not met.'
+    END;
+SELECT 'Tear down: Remove session (floor 3, room 4, 17/11/2021, 1800, booker id 12) '
+        'and (floor 3, room 4, 17/11/2021, 1900, booker id 12)';
+SELECT unbook_room(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 12);
+SELECT
+    CASE
+        NOT is_existing_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME)
+        AND NOT is_existing_session(3, 4, '17/11/2021'::DATE, '19:00'::TIME)
+    WHEN TRUE
+        THEN 'Tear down successful.'
+    ELSE
+        'Tear down unsuccessful.'
+    END;
+
+-- leave one self-booked session
+SELECT 'Test case: leave one self-booked session';
+SELECT 'Precondition: Session (floor 3, room 4, 17/11/2021, 1800, booker id 45) exists';
+SELECT book_room(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 45);
+SELECT
+    CASE
+        is_existing_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Precondition: Join (floor 3, room 4, 17/11/2021, 1800, joiner id 45) exists';
+SELECT
+    CASE
+        is_joining_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME, 45)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected result: Successfully return after leaving one session, and the session is unbooked.';
+SELECT leave_meeting(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '19:00'::TIME, 45);
+SELECT 'Postcondition: Join (floor 3, room 4, 17/11/2021, 1800, joiner id 45) does not exist';
+SELECT
+    CASE
+        NOT is_joining_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME, 45)
+    WHEN TRUE
+        THEN 'Postcondition met.'
+    ELSE
+        'Postcondition not met.'
+    END;
+SELECT 'Postcondition: Session (floor 3, room 4, 17/11/2021, 1800, booker id 45) does not exist';
+SELECT
+    CASE
+        NOT is_existing_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME)
+    WHEN TRUE
+        THEN 'Postcondition met.'
+    ELSE
+        'Postcondition not met.'
+    END;
+
+-- leave multiple self-booked sessions
+SELECT 'Test case: leave multiple self-booked sessions';
+SELECT 'Precondition: Session (floor 3, room 4, 17/11/2021, 1800, booker id 45) '
+        'and (floor 3, room 4, 17/11/2021, 1900, booker id 45) exist';
+SELECT book_room(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 45);
+SELECT
+    CASE
+        is_existing_meeting(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Precondition: Join (floor 3, room 4, 17/11/2021, 1800, joiner id 45) '
+        'and (floor 3, room 4, 17/11/2021, 1900, joiner id 45) exist';
+SELECT
+    CASE
+        is_joining_entire_duration(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 45)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected result: Successfully return after leaving multiple sessions.';
+SELECT leave_meeting(3, 4, '17/11/2021'::DATE, '18:00'::TIME, '20:00'::TIME, 45);
+SELECT 'Postcondition: Join (floor 3, room 4, 17/11/2021, 1800, joiner id 45) '
+        'and (floor 3, room 4, 17/11/2021, 1900, joiner id 45) do not exist';
+SELECT
+    CASE
+        NOT is_joining_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME, 45)
+        AND NOT is_joining_session(3, 4, '17/11/2021'::DATE, '19:00'::TIME, 45)
+    WHEN TRUE
+        THEN 'Postcondition met.'
+    ELSE
+        'Postcondition not met.'
+    END;
+SELECT 'Postcondition: Session (floor 3, room 4, 17/11/2021, 1800, booker id 45) '
+        'and (floor 3, room 4, 17/11/2021, 1900, booker id 45) do not exist';
+SELECT
+    CASE
+        NOT is_existing_session(3, 4, '17/11/2021'::DATE, '18:00'::TIME)
+        AND NOT is_existing_session(3, 4, '17/11/2021'::DATE, '19:00'::TIME)
+    WHEN TRUE
+        THEN 'Postcondition met.'
+    ELSE
+        'Postcondition not met.'
+    END;
