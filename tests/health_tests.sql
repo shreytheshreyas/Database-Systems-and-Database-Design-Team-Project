54ef779e1477d7c0ec59b678bc165edadaab2080
Sam Chris <thesamchris@gmail.com>
2021-11-06 22:18:46 +0800

    health_tests join_meeting tbc

--- tests/health_tests.sql
+++ tests/health_tests.sql
@@ -9,18 +9,24 @@ CALL declare_health (60, '6/11/2021'::DATE, 20);
 CALL declare_health (60, '6/11/2022'::DATE, 36.4);
 
 -- declare_health, works
-CALL declare_health (61, '6/11/2021'::DATE, 36.4); 
+CALL declare_health (1, '6/11/2021'::DATE, 36.4); 
 
 -- contact_tracing, employee that is well
 -- employee did attend meetings in the past w three other employees and is sick
--- all 4 employees join meeting room: 3 floor: 2  date: 2021-01-11 
+-- all 4 employees from same department join meeting room: 3 floor: 2  date: 2021-01-11 
 
-CALL join_meeting (3, 2, '1/11/2021'::DATE, '11:00'::TIME, '12:00'::TIME, 1);
-CALL join_meeting (3, 2, '1/11/2021'::DATE, '11:00'::TIME, '12:00'::TIME, 2);
-CALL join_meeting (3, 2, '1/11/2021'::DATE, '11:00'::TIME, '12:00'::TIME, 3);
-CALL join_meeting (3, 2, '1/11/2021'::DATE, '11:00'::TIME, '12:00'::TIME, 4);
+SELECT join_meeting (3, 2, '1/11/2021'::DATE, '11:00'::TIME, '12:00'::TIME, 2);
+SELECT join_meeting (3, 2, '1/11/2021'::DATE, '11:00'::TIME, '12:00'::TIME, 12);
+SELECT join_meeting (3, 2, '1/11/2021'::DATE, '11:00'::TIME, '12:00'::TIME, 22);
+SELECT join_meeting (3, 2, '1/11/2021'::DATE, '11:00'::TIME, '12:00'::TIME, 32);
+
+-- -- meeting that has not been approved yet
+-- SELECT join_meeting (4, 3, '12/11/2021'::DATE, '16:00'::TIME, '17:00'::TIME, 3);
+-- SELECT join_meeting (4, 3, '12/11/2021'::DATE, '16:00'::TIME, '17:00'::TIME, 9);
+-- SELECT join_meeting (4, 3, '12/11/2021'::DATE, '16:00'::TIME, '17:00'::TIME, 19);
+-- SELECT join_meeting (4, 3, '12/11/2021'::DATE, '16:00'::TIME, '17:00'::TIME, 29);
 
 -- make one employee sick
-CALL declare_health (1, '6/11/2021'::DATE, 40); 
+CALL declare_health (3, '5/11/2021'::DATE, 38); 
 -- run contact_tracing
-CALL contact_tracing (1)
+SELECT contact_tracing (1);
