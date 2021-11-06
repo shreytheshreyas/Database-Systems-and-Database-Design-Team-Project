f0cd9cfe244deb3f9430f8bd1c08736c69784cfa
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-11-07 06:51:39 +0800

    Add tests for approve_meeting

--- core_test.sql
+++ core_test.sql
@@ -334,3 +334,138 @@ SELECT
     ELSE
         'Postcondition not met.'
     END;
+
+/**
+ * approve_meeting test cases
+ */
+SELECT '##### Test cases for approve_meeting #####';
+
+-- non-existent employee
+SELECT 'Test case: non-existent employee';
+SELECT 'Expected error: This employee does not exist.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '15:00'::TIME, '16:00'::TIME, 61);
+
+-- retired employee
+SELECT 'Test case: retired employee';
+SELECT 'Expected error: This employee is already retired.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '15:00'::TIME, '16:00'::TIME, 60);
+
+-- junior employee
+SELECT 'Test case: junior employee';
+SELECT 'Expected error: This employee is not a manager; only managers can approve a meeting.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '15:00'::TIME, '16:00'::TIME, 59);
+
+-- senior employee
+SELECT 'Test case: senior employee';
+SELECT 'Expected error: This employee is not a manager; only managers can approve a meeting.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '15:00'::TIME, '16:00'::TIME, 50);
+
+-- different department
+SELECT 'Test case: different department';
+SELECT 'Expected error: The manager approving the meeting must be from the same department that the meeting room belongs to.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '15:00'::TIME, '16:00'::TIME, 2);
+
+-- not on-the-hour start time
+SELECT 'Test case: not on-the-hour start time';
+SELECT 'Expected error: All hours must be exactly on the hour.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '15:01'::TIME, '16:00'::TIME, 1);
+SELECT 'Expected error: All hours must be exactly on the hour.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '14:59'::TIME, '16:00'::TIME, 1);
+
+-- not on-the-hour end time
+SELECT 'Test case: not on-the-hour end time';
+SELECT 'Expected error: All hours must be exactly on the hour.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '15:00'::TIME, '16:01'::TIME, 1);
+SELECT 'Expected error: All hours must be exactly on the hour.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '15:00'::TIME, '15:59'::TIME, 1);
+
+-- non-existent meeting
+SELECT 'Test case: non-existent meeting';
+SELECT 'Expected error: This meeting does not exist.';
+SELECT approve_meeting(0, 1, '15/11/2021'::DATE, '15:00'::TIME, '16:00'::TIME, 1);
+SELECT 'Expected error: This meeting does not exist.';
+SELECT approve_meeting(1, 0, '15/11/2021'::DATE, '15:00'::TIME, '16:00'::TIME, 1);
+SELECT 'Expected error: This meeting does not exist.';
+SELECT approve_meeting(1, 1, '15/11/2020'::DATE, '15:00'::TIME, '16:00'::TIME, 1);
+SELECT 'Expected error: This meeting does not exist.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '14:00'::TIME, '15:00'::TIME, 1);
+SELECT 'Expected error: This meeting does not exist.';
+SELECT approve_meeting(1, 1, '15/11/2021'::DATE, '14:00'::TIME, '17:00'::TIME, 1);
+
+-- past start time
+SELECT 'Test case: past start time';
+SELECT 'Precondition: Session (floor 1, room 1, 6/11/2020, 0600, booker id 2) exists';
+SELECT
+    CASE
+        is_existing_session(1, 1, '6/11/2021'::DATE, '6:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected error: This meeting would have already started or possibly even finished.';
+SELECT approve_meeting(1, 1, '6/11/2021'::DATE, '06:00'::TIME, '07:00'::TIME, 1);
+
+-- valid completely unapproved meeting
+SELECT 'Test case: valid completely unapproved meeting';
+SELECT 'Precondition: Meeting (floor 1, room 1, 6/11/2020, 0600, 0900, booker id 2) exists';
+SELECT book_room(1, 1, '6/12/2021'::DATE, '7:00'::TIME, '10:00'::TIME, 2);
+SELECT
+    CASE
+        is_existing_meeting(1, 1, '6/12/2021'::DATE, '7:00'::TIME, '10:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected result: Successfully approves meeting.';
+SELECT approve_meeting(1, 1, '6/12/2021'::DATE, '7:00'::TIME, '10:00'::TIME, 1);
+SELECT 'Tear down: Remove meeting (floor 1, room 1, 6/11/2020, 0600, 0900, booker id 2)';
+SELECT unbook_room(1, 1, '6/12/2021'::DATE, '7:00'::TIME, '10:00'::TIME, 2);
+SELECT
+    CASE
+        NOT is_existing_session(1, 1, '6/12/2021'::DATE, '7:00'::TIME)
+        AND NOT is_existing_session(1, 1, '6/12/2021'::DATE, '8:00'::TIME)
+        AND NOT is_existing_session(1, 1, '6/12/2021'::DATE, '9:00'::TIME)
+    WHEN TRUE
+        THEN 'Tear down successful.'
+    ELSE
+        'Tear down unsuccessful.'
+    END;
+
+-- partially approved meeting
+SELECT 'Test case: partially approved meeting';
+SELECT 'Precondition: Meeting (floor 1, room 1, 6/11/2020, 0600, 0900, booker id 2) exists';
+SELECT book_room(1, 1, '6/12/2021'::DATE, '6:00'::TIME, '9:00'::TIME, 2);
+SELECT
+    CASE
+        is_existing_meeting(1, 1, '6/12/2021'::DATE, '6:00'::TIME, '9:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Precondition: Session (floor 1, room 1, 6/11/2020, 0700, booker id 2) is already approved';
+SELECT approve_meeting(1, 1, '6/12/2021'::DATE, '7:00'::TIME, '8:00'::TIME, 1);
+SELECT
+    CASE
+        is_approved_session(1, 1, '6/12/2021'::DATE, '7:00'::TIME)
+    WHEN TRUE
+        THEN 'Precondition met.'
+    ELSE
+        'Precondition not met.'
+    END;
+SELECT 'Expected error: None of the sessions in the meeting must already be approved.';
+SELECT approve_meeting(1, 1, '6/12/2021'::DATE, '6:00'::TIME, '9:00'::TIME, 1);
+SELECT 'Tear down: Remove meeting (floor 1, room 1, 6/11/2020, 0600, 0900, booker id 2)';
+SELECT unbook_room(1, 1, '6/12/2021'::DATE, '6:00'::TIME, '9:00'::TIME, 2);
+SELECT
+    CASE
+        NOT is_existing_session(1, 1, '6/12/2021'::DATE, '6:00'::TIME)
+        AND NOT is_existing_session(1, 1, '6/12/2021'::DATE, '7:00'::TIME)
+        AND NOT is_existing_session(1, 1, '6/12/2021'::DATE, '8:00'::TIME)
+    WHEN TRUE
+        THEN 'Tear down successful.'
+    ELSE
+        'Tear down unsuccessful.'
+    END;
